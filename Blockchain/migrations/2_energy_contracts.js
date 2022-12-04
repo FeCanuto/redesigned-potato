6c803f7406d9fc1383b8d776d35d3bbd84c01ae5
FeCanuto <felipee.canuto@hotmail.com>
2022-12-04 14:04:28 -0300

    Alterado nome da migration

---
+++ Blockchain/migrations/2_energy_contracts.js
@@ -0,0 +1,9 @@
+const RenewableEnergyToken = artifacts.require('RenewableEnergyToken');
+const EnergyTrade = artifacts.require('EnergyTrade');
+
+module.exports = async function (deployer) {
+    await deployer.deploy(RenewableEnergyToken);
+    const token = await RenewableEnergyToken.deployed();
+  
+    await deployer.deploy(EnergyTrade, token.address);
+  };
